12e80e2dbf7b46be3783dab4320c61f65fc0c0dc
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2025-05-08 19:56:06 -0500

    Oopsie poopsie!

---
+++ mods/grandmaster/logic/misc.lua
@@ -0,0 +1,12 @@
+--Thanks to OgelGames and BuckarooBanzai for this temp fix
+
+local level = tonumber(minetest.settings:get("light_level")) or 2
+
+minetest.register_on_mods_loaded(function ()
+	for i, def in pairs(minetest.registered_nodes) do
+		local light_source = def.light_source
+		if light_source == nil or light_source < level then
+			minetest.override_item(i, { light_source = level })
+		end
+	end
+end)
